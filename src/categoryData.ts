1a0ef0a80df665e60d5949a5e6f4f9f53081bc87
Guangyin Ye <gy020306@uw.edu>
2023-08-06 15:05:33 -0700

    added restaurants auto fill

--- src/categoryData.ts
+++ src/categoryData.ts
@@ -9,6 +9,6 @@ subCategories.set("买菜", ["Fred Meyer", "QFC", "Safeway", "百家", "Costco",
 subCategories.set("加油", ["Costco", "ARCO", "其它"]);
 subCategories.set("租房", ["全部"]);
 subCategories.set("Utilities", ["网络", "电费", "水费和垃圾"]);
-subCategories.set("饭店", []);
+subCategories.set("饭店", ["McDonald's"]);
 subCategories.set("Outlets", ["Gap"]);
 subCategories.set("家装家具", ["IKEA", "Home Depot", "Fred Meyer"]);
