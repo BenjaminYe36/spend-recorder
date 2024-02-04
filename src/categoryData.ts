9ed1b3123c8cd6d15258229c32422247b938056f
Guangyin Ye <gy020306@uw.edu>
2024-02-04 15:33:01 -0800

    updated autofill info

--- src/categoryData.ts
+++ src/categoryData.ts
@@ -1,4 +1,4 @@
-export const categories = ["买菜", "加油", "租房", "Utilities", "饭店", "Outlets", "家装家具"]
+export const categories = ["买菜", "加油", "租房", "Utilities", "饭店", "Outlets", "家装家具", "Gym"]
     .map((val) => {
         return {displayName: val};
     });
@@ -9,6 +9,7 @@ subCategories.set("买菜", ["Fred Meyer", "QFC", "Safeway", "百家", "Costco",
 subCategories.set("加油", ["Costco", "ARCO", "其它"]);
 subCategories.set("租房", ["全部"]);
 subCategories.set("Utilities", ["网络", "电费", "水费和垃圾"]);
-subCategories.set("饭店", ["McDonald's"]);
+subCategories.set("饭店", ["McDonald's", "Chipotle"]);
 subCategories.set("Outlets", ["Gap"]);
 subCategories.set("家装家具", ["IKEA", "Home Depot", "Fred Meyer"]);
+subCategories.set("Gym", ["LA Fitness"]);
